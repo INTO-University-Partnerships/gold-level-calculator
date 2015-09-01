c4ebc35880a967085394a767c4174911e8fe81e4
Mike McGowan <mike.mcgowan@into.uk.com>
2015-09-01 09:40:48 +0100

    Tidied up Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -14,33 +14,35 @@ import qualified Data.Vector as V
 processMatrix :: (Either String (V.Vector ScoreTarget), Either String (V.Vector ScoreGroup)) -> IO (V.Vector ScoreTarget, V.Vector ScoreGroup)
 processMatrix eithers = do
     case fst eithers of
-        Left e -> do
+        Left  evst -> showErrorAndReturnEmptyVectors evst
+        Right rvst -> do
+            case snd eithers of
+                Left  evsg -> showErrorAndReturnEmptyVectors evsg
+                Right rvsg -> return $ (rvst, rvsg)
+    where
+        showErrorAndReturnEmptyVectors e = do
             putStrLn e
             return $ (V.empty, V.empty)
-        Right r -> do
-            case snd eithers of
-                Left e' -> do
-                    putStrLn e'
-                    return $ (V.empty, V.empty)
-                Right r' -> return $ (r, r')
 
 getIELTSLevelDataMap :: IO (Maybe IELTSLevelDataMap)
 getIELTSLevelDataMap = do
     csvData <- BL.readFile "data/GOLD levels.csv"
     case decode NoHeader csvData of
-        Left  _ -> return Nothing
+        Left e -> do
+            putStrLn e
+            return Nothing
         Right m -> do
-            (a, b) <- (processMatrix . parseMatrix) m
-            return $ Just $ toIELTSLevelDataMap a b
+            (vst, vsg) <- (processMatrix . parseMatrix) m
+            return $ Just $ toIELTSLevelDataMap vst vsg
 
 main :: IO ()
 main = do
-    ldm <- getIELTSLevelDataMap
-    case ldm of
+    ieltsLevelDataMap <- getIELTSLevelDataMap
+    case ieltsLevelDataMap of
         Nothing -> putStrLn "Something went wrong"
-        Just ieltsLevelDataMap -> do
-            forM_ (M.keys ieltsLevelDataMap) $ \k -> do
-                case M.lookup k ieltsLevelDataMap of
+        Just ieltsLevelDataMap' -> do
+            forM_ (M.keys ieltsLevelDataMap') $ \k -> do
+                case M.lookup k ieltsLevelDataMap' of
                     Just (IELTSLevelData st sgm) -> do
                         putStrLn $ "IELTS level data for " ++ show k
                         putStrLn $ show st
